c872de7c93b3e9c06ecee197c19ad2dfe108949a
Shimin Yang <yangshimin@youzan.com>
2018-09-27 16:58:00 +0800

    Bump to 1.6.1-yz-1.0-SNAPSHOT

--- tools/change-version.sh
+++ tools/change-version.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 ################################################################################
 
-OLD="1.3-SNAPSHOT"
-NEW="1.4-SNAPSHOT"
+OLD="1.6.1"
+NEW="1.6.1-yz-1.0-SNAPSHOT"
 
 
 HERE=` basename "$PWD"`
